a03350f595359c3c8c34644e28994e7b69228e04
JihoKevin <jhshin925@gmail.com>
2021-09-16 17:58:33 +0900

    deleted two files

--- app/src/main/java/kevin/android/fifaonline/repository/Repository.kt
+++ app/src/main/java/kevin/android/fifaonline/repository/Repository.kt
@@ -1,16 +1,8 @@
 package kevin.android.fifaonline.repository
 
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
-import kevin.android.fifaonline.BaseResponse
 import kevin.android.fifaonline.api.Api
 import kevin.android.fifaonline.model.Model
-import kevin.android.fifaonline.api.Client
-import retrofit2.Callback
-import retrofit2.HttpException
-import retrofit2.Response
 import javax.inject.Inject
 
 // repository를 요래 쓰는게 맞나..?
--- app/src/main/java/kevin/android/fifaonline/util/Constant.kt
+++
@@ -1,9 +0,0 @@
-package kevin.android.fifaonline.util
-
-/**
- * Created by JihoKevin.
- * User: sinjiho
- * Date: 2021/09/16
- * Time: 10:37 오전
- */
-
