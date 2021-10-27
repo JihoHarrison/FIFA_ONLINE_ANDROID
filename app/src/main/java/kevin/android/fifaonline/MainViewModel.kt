4821290ea3da573fa080012d3fe5c9c8466acb8b
JihoKevin <jhshin925@gmail.com>
2021-10-27 18:39:25 +0900

    Match list sorted

--- app/src/main/java/kevin/android/fifaonline/MainViewModel.kt
+++ app/src/main/java/kevin/android/fifaonline/MainViewModel.kt
@@ -24,31 +24,15 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
 
     private val disposables by lazy { CompositeDisposable() }
-
     var matchLists: MutableLiveData<List<MatchDTO>> = MutableLiveData<List<MatchDTO>>()
-    var matchArrayLists: MutableLiveData<ArrayList<MatchDTO>> = MutableLiveData<ArrayList<MatchDTO>>()
-    private lateinit var lists: MutableList<MatchDTO>
-    private lateinit var matchIdLists: List<String>
 
     private val fifaProcessor: BehaviorProcessor<UserModel> =
         BehaviorProcessor.create()
     private val matchIdProcessor: BehaviorProcessor<List<String>> =
         BehaviorProcessor.create()
 
-
-    private var matchInfoProcessor: PublishSubject<List<MatchDTO>> =
-        PublishSubject.create()
-
-    var matchListsProcess: Observable<List<MatchDTO>> = matchInfoProcessor
     val userNickName: Flowable<String> = fifaProcessor.map { it.nickname }
     val userLevel: Flowable<String> = fifaProcessor.map { it.level.toString() }
-//    val matchInfoProcess: Single<List<MatchDTO>> = matchInfoProcessor
-//    val user01: Flowable<String> = matchInfoProcessor.map { it.matchInfo[0].nickname }
-//    val user02: Flowable<String> = matchInfoProcessor.map { it.matchInfo[1].nickname }
-//    val user01Score: Flowable<Int> = matchInfoProcessor.map { it.matchInfo[0].shoot.goalTotal }
-//    val user02Score: Flowable<Int> = matchInfoProcessor.map { it.matchInfo[1].shoot.goalTotal }
-//    val user01Result: Flowable<String> = matchInfoProcessor.map { it.matchInfo[0].matchDetail.matchResult }
-//    val user02Result: Flowable<String> = matchInfoProcessor.map { it.matchInfo[1].matchDetail.matchResult }
 
     fun getFifaInfo(nickname: String) {
         repository.getModel(nickname).subscribeOn(Schedulers.io()).observeOnMain().subscribe(
@@ -65,39 +49,26 @@ class MainViewModel @Inject constructor(private val repository: Repository) : Vi
         repository.getOfficialMatchIdRepo(accessId).subscribeOn(Schedulers.io()).observeOnMain().subscribe(
             {
                 matchIdProcessor.offer(it)
-                matchIdLists = it
+                for(element in it){
+                    getOfficialMatchInfo(element)
+                }
             }, {
                 Log.d("error", it.message.toString())
             }
         ).addToDisposables()
     }
 
-    fun getOfficialMatchInfo() {
+    fun getOfficialMatchInfo(matchId: String) {
 
-        matchIdLists.forEach {
-            repository.getMatchInfoRepo(it)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-//                matchInfoProcessor.offer(it)
+        repository.getMatchInfoRepo(matchId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                matchLists.setValue((matchLists.value?.sortedByDescending { it.matchDate } ?: emptyList()) + it)
+            }, {
 
-                    //lists.add(it)
-                    //Log.d("matchIds", lists.toString())
-                    Log.d("helloworld", it.toString())
-                    //matchLists.value = it
-                    //matchIdLists = it
-
-                    //matchArrayLists.value?.add(it)
-                    //matchInfoProcessor.onNext(it)
-                    matchLists.postValue((matchLists.value ?: emptyList()) + it)
-
-                }, {
-
-                }
-                ).addToDisposables()
-        }
-
+            }
+            ).addToDisposables()
     }
-
     private fun Disposable.addToDisposables(): Disposable = addTo(disposables)
 }
--- app/src/main/java/kevin/android/fifaonline/presentation/match/MatchActivity.kt
+++ app/src/main/java/kevin/android/fifaonline/presentation/match/MatchActivity.kt
@@ -28,63 +28,15 @@ class MatchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_match)
 
-        var shootDetail: List<ShootDetailDTO> = listOf(ShootDetailDTO(1, 1, 1))
-        var player: List<PlayerDTO> = listOf(PlayerDTO(1, 1, 1))
-
-        var matchInfos: List<MatchInfoDTO> = listOf(
-            MatchInfoDTO(
-                "qewr",
-                "jiho",
-                MatchDetailDTO(
-                    2021,
-                    "패", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.1,
-                    "hello"
-                ),
-                ShootDTO(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-                shootDetail,
-                PassDTO(1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-                DefenceDTO(1, 1, 2, 1),
-                player
-            ), MatchInfoDTO(
-                "qewr",
-                "kevin",
-                MatchDetailDTO(
-                    2021,
-                    "승", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.1,
-                    "hello"
-                ),
-                ShootDTO(1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1),
-                shootDetail,
-                PassDTO(1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-                DefenceDTO(1, 1, 2, 1),
-                player
-            )
-        )
-
-        var matchs: List<MatchDTO> = listOf(
-            MatchDTO("he", "he", 1, matchInfos),
-            MatchDTO("he", "he", 1, matchInfos),
-            MatchDTO("he", "he", 1, matchInfos),
-            MatchDTO("he", "he", 1, matchInfos),
-            MatchDTO("he", "he", 1, matchInfos),
-        )
-
         Log.d("hey", viewModel.getFifaInfo("꼬솜슈터").toString())
-        viewModel.getOfficialMatchInfo()
 
         viewModel.matchLists.observe(this, Observer {
+
             adapter = MatchResultAdapter(it)
+
+            it.sortedByDescending { it.matchDate }
             binding.rcMatchList.adapter = adapter
         })
-
-//        viewModel.matchListsProcess.firstOrError()
-//            .observeOnMain()
-//            .subscribeWithErrorLogger {
-//                //viewModel.orderMatchLists(it)
-//                adapter = MatchResultAdapter(it)
-//                binding.rcMatchList.adapter = adapter
-//            }
-
     }
 
 }
